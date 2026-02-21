a7c5d9a90d009bb96ec59cb4e188226b5ea9ed2b
portside0 <studies0and0observations@gmail.com>
2026-02-21 23:13:11 +0530

    Add files via upload

---
+++ eratosthenes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+	int n;
+	
+	scanf("%i", &n);
+	
+	bool prime[n + 1];
+	
+	for (int i = 0; i <= n; i++)
+	{
+		prime[i] = true;
+	}
+	
+	prime[0] = false;
+	prime[1] = false;
+	
+	for (int j = 2; j * j <= n; j++)
+	{
+		if (prime[j] == true)
+		{
+			for (int k = j * j; k <= n; k = k + j)
+			{
+				prime[k] = false;
+			}
+		}
+	}
+	
+	int m = 1;
+	
+	for (int l = 0; l <= n; l++)
+	{	
+		if (prime[l] == true)
+		{
+			printf("%i - %i\n", m, l);
+			m++;
+		}
+	}
+	
+	printf("\n");
+}
